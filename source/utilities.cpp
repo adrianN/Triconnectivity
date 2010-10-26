be0a07299b7d8a45c5fd65984d930559c15a14b4
Adrian <adrian_neumann@gmx.de>
2010-10-26 11:09:38 +0200

    made use of undocumented node->id(), added ordering labels to edges

--- source/utilities.cpp
+++ source/utilities.cpp
@@ -1,34 +1,24 @@
 #include "triconnectivity.hpp"
 #include <ostream>
-#include <sstream>
 
 using namespace leda;
 using std::endl;
 
-void to_dot(const ugraph& g, std::ostream& output) {
-	edge e;
-	std::ostringstream out;
-	out << "graph G {" << endl;
+void to_dot(const ugraph& g, std::ostream& out) {
 
-	forall_edges(e,g) {
-		node src = source(e);
-		node trg = target(e);
+	out << "digraph G {" << endl;
+	node n;
+	forall_nodes(n,g) {
 
-		out << "\tnode";
-		g.print_node(src,out);
-		out << " -- node";
-		g.print_node(trg,out);
-		out << endl;
-	}
+		out << "\tnode" << n->id() << " [label = \"" << n->id() << "\"]" << endl;
 
-	out << "}" << endl;
-	std::string s = out.str();
-	// Since dot doesn't like [], we have to replace that. There should be a fancy way to do global replace in strings
-	for(unsigned int i=0; i<s.length(); i++) {
-		if (s[i]=='[' || s[i] == ']') {
-			s[i] = '_';
+		node u;
+		unsigned int i=0;
+		forall_adj_nodes(u,n) {
+			out << "\tnode" << n->id() << " -> " << "node" << u->id() << " [label = \"" << ++i << "\"]" << endl;
 		}
 	}
 
-	output << s;
+	out << "}" << endl;
+
 }
