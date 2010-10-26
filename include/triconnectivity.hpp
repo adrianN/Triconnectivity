6ab1332b2dbd06fcf1494847b9ecb9f6e88598ae
Adrian <adrian_neumann@gmx.de>
2010-10-26 10:50:39 +0200

    added naive_separation_pairs, made use of auto_ptr

--- include/triconnectivity.hpp
+++ include/triconnectivity.hpp
@@ -9,7 +9,12 @@
 #define TRICONNECTIVITY_HPP_
 
 #include "LEDA/graph/ugraph.h"
+#include "LEDA/core/tuple.h"
+#include "LEDA/core/list.h"
+#include <memory>
+
 
 bool naive_is_triconnected(leda::ugraph&);
+std::auto_ptr<leda::list<leda::two_tuple<leda::node,leda::node> > > naive_separation_pairs(leda::ugraph& g);
 
 #endif /* TRICONNECTIVITY_HPP_ */
--- source/triconnectivity.cpp
+++ source/triconnectivity.cpp
@@ -1,6 +1,8 @@
 #include "triconnectivity.hpp"
 #include "dfs.hpp"
 
+using std::auto_ptr;
+
 using namespace leda;
 
 /* A graph is triconnected if there is no pair of nodes u,v s.t.
@@ -8,37 +10,38 @@ using namespace leda;
  * there exists a pair of nodes x,y s.t. all paths between x,y pass through u or v.
  */
 bool naive_is_triconnected(ugraph& g) {
+	if (!is_connected(g)) return false;
+	auto_ptr<list<two_tuple<node,node> > > sep_pairs =naive_separation_pairs(g);
+	bool b = sep_pairs->length()==0;
 
-	if (!is_connected(g))
-		return false;
+	return b;
+}
+
+
+/* Return a list of all separation pairs of g. Precondition: g is connected */
+auto_ptr<list<two_tuple<node,node> > > naive_separation_pairs(ugraph& g) {
+	assert(is_connected(g));
 	node u;
+	auto_ptr<list<two_tuple<node,node> > > pairs(new list<two_tuple<node,node> >());
 	forall_nodes(u,g) {
-		int degree = g.degree(u);
 		list<edge> adj_edges = g.adj_edges(u);
 		g.hide_node(u);
 
 		node v;
 		forall_nodes(v,g) {
-			int degree = g.degree(v);
 			list<edge> adj_edges = g.adj_edges(v);
 			g.hide_node(v);
 
 			if (!is_connected(g)) { //u,v is a separation pair
-				g.restore_all_nodes();
-				g.restore_all_edges();
-				return false;
+				pairs->append(two_tuple<node,node>(u,v));
 			}
 
 			g.restore_node(v);
 			g.restore_edges(adj_edges);
-			assert(!g.is_hidden(v));
-			assert(degree==g.degree(v));
-
 		}
 		g.restore_node(u);
 		g.restore_edges(adj_edges);
-		assert(degree==g.degree(u));
 
 	}
-	return true;
+	return pairs;
 }
