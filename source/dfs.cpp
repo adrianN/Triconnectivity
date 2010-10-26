40d05019c1c44cd21f9cfafc36f771999f728475
Adrian <adrian_neumann@gmx.de>
2010-10-26 11:33:54 +0200

    added more consts

--- source/dfs.cpp
+++ source/dfs.cpp
@@ -6,21 +6,21 @@ using namespace leda;
 /* Does a DFS to determine whether the graph is connected or not */
 bool is_connected(const ugraph& g) {
 	assert(g.number_of_nodes()>=0);
-	unsigned int number_of_nodes = g.number_of_nodes();
+	const unsigned int number_of_nodes = g.number_of_nodes();
 	node* stack = new node[number_of_nodes];
-	node* base = stack;
+	const node* base = stack;
 	*stack = g.first_node();
 	assert(!g.is_hidden(*stack));
 
 	node_array<bool> visited(g,false);
-	assert(visited[*stack]==false);
+	assert(visited[*stack]==false); //Leda seems buggy around here
 	visited[*stack] = true;
 
 	unsigned int number_seen=1;
 
 	while(stack>=base) {
 		//pop from stack
-		/*const*/ node current = *stack;
+		const node current = *stack;
 		stack--;
 		node next_node;
 
