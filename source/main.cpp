29e56af3cef42b53e19e8847d1cb10e21f6e5d92
Adrian <adrian_neumann@gmx.de>
2011-01-19 13:12:46 +0100

    interlocking intervals seems to work

--- source/main.cpp
+++ source/main.cpp
@@ -33,26 +33,35 @@ void to_file(const ugraph& g, const char* name) {
 int main(int argc, char* argv[]) {
 
 	interval<int>* intervals[7];
-	unsigned int b[7][2] = {
+	unsigned int b[8][2] = {
 			{0,3},
 			{1,3},
 			{2,6},
 			{2,4},
 			{5,8},
 			{6,9},
-			{7,8}
+			{7,8},
+			{20,25}
 	};
 
 	slist<interval<int>* > asc;
 	slist<interval<int>* > dsc;
 	slist<slist<interval<int>*>* > equiv;
 
-	for(unsigned int i = 0; i<7; i++) {
+
+	for(unsigned int i = 0; i<8; i++) {
 		interval<int>* it =  new interval<int>(b[i],i);
 		intervals[i] = it;
 		asc.append(it);
 	}
 
+	slist<interval<int>*> one;
+	one.append(intervals[0]);
+	one.append(intervals[1]);
+	one.append(intervals[7]);
+	equiv.append(&one);
+
+	dsc.append(intervals[7]);
 	dsc.append(intervals[5]);
 	dsc.append(intervals[6]);
 	dsc.append(intervals[4]);
